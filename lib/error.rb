0afa033afec7ee701ecb074771676c558c1ae66f
chirchir <chirchir7370@gmail.com>
2020-06-13 00:16:40 +0300

    add display_error method in Error class

--- lib/error.rb
+++ lib/error.rb
@@ -6,4 +6,12 @@ class ErrorHandler
     @error_arr = []
     @data = data
   end
+
+  def display_errors
+    check_missing_semi_colon(str, idx, arr)
+    check_space_before_semi_colon(str, idx, arr)
+    check_indentations(str, idx, arr)
+    check_single_space_after_after_colon(str, idx, arr)
+    check_trailing_white_space(str, idx, arr)
+  end
 end
