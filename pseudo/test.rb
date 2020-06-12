56e7d0f37f47196f8323ddf8a98063b8138a178a
chirchir <chirchir7370@gmail.com>
2020-06-12 21:43:51 +0300

    add test for start_of_style_block

--- pseudo/test.rb
+++ pseudo/test.rb
@@ -15,7 +15,7 @@
 #     #([a-zA-Z])+(:)
 # #no_trailing_white_space?
 #  #({$) | (;"$) | (}$)
-#  def start_of_style_block?(str)
+#  def start_of_style_block(str)
 #   !!(str=~/^(#|\.)+([a-zA-Z0-9\-_]+)+(\s*\.\s*|>|:*)+(\w+)+(\s*)+({\s*)/ ||
 #   str=~/^([a-zA-Z0-9\-_]+)+(\.|\s*>\s*|:+)+([a-zA-Z0-9\-_]+)+(\s*{\s*)/ || str=~/^\n/)
 #  end
