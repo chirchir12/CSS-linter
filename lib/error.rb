4620a41a8b0b82dc73b314b0cb7b189c440507dd
chirchir <chirchir7370@gmail.com>
2020-06-13 00:13:33 +0300

    add contructor in error class

--- lib/error.rb
+++ lib/error.rb
@@ -1,4 +1,9 @@
 require_relative './modules/error.rb'
 class ErrorHandler
   include ErrorModule
+  attr_reader :error_arr, :data
+  def initiliaze(data)
+    @error_arr = []
+    @data = data
+  end
 end
